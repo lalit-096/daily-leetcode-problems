e3c804a230b47431a1f1ad5a57109d3fcb1c1c20
Lalit-096 <mishralalit595@gmail.com>
2026-07-18 03:33:05 +0530

    FIX: fixed the issue of MLE.

--- 2026/July/18_july_2026/3312_Sorted GCD Pair Queries.py
+++ 2026/July/18_july_2026/3312_Sorted GCD Pair Queries.py
@@ -1,16 +1,50 @@
 from typing import List
-from math import gcd
+from bisect import bisect_right
 
 class Solution:
     def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
-        gcdPairs = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                gcdPairs.append(gcd(nums[i], nums[j]))
+        max_val = max(nums)
 
-        gcdPairs.sort()
-        
-        return [gcdPairs[q] for q in queries]
+        # Frequency of each number
+        freq = [0] * (max_val + 1)
+        for num in nums:
+            freq[num] += 1
+
+        # divisible[g] = how many numbers are divisible by g
+        divisible = [0] * (max_val + 1)
+
+        for g in range(1, max_val + 1):
+            for multiple in range(g, max_val + 1, g):
+                divisible[g] += freq[multiple]
+
+        # pairs[g] = number of pairs whose gcd is exactly g
+        pairs = [0] * (max_val + 1)
+
+        for g in range(max_val, 0, -1):
+            cnt = divisible[g]
+            pairs[g] = cnt * (cnt - 1) // 2
+
+            for multiple in range(2 * g, max_val + 1, g):
+                pairs[g] -= pairs[multiple]
+
+        # Prefix sums over gcd values
+        prefix = []
+        gcd_values = []
+
+        running = 0
+        for g in range(1, max_val + 1):
+            if pairs[g]:
+                running += pairs[g]
+                prefix.append(running)
+                gcd_values.append(g)
+
+        # Answer queries
+        ans = []
+        for q in queries:
+            idx = bisect_right(prefix, q)
+            ans.append(gcd_values[idx])
+
+        return ans
 
 
 
